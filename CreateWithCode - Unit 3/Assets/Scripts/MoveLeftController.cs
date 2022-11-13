f3918a105d829937cc9e9065ee0d9f15d5bc2c7e
zhenxxx7 <jordy.zeventh@gmail.com>
2022-11-14 00:02:42 +0700

    add files Unit 3

---
+++ CreateWithCode - Unit 3/Assets/Scripts/MoveLeftController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftController : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController && !playerController.isGameOver)
+        {
+            transform.Translate(Vector3.left * playerController.GetSpeed() * Time.deltaTime);
+        }
+    }
+}
---
+++ CreateWithCode - Unit 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject obstaclePrefab;
+    [SerializeField] private Vector3 spawnPosition = new Vector3(0, 0, 0);
+    [SerializeField] private float spawnDelayMin = 0;
+    [SerializeField] private float spawnDelayMax = 0;
+
+    private PlayerController playerController;
+
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        SpawnObstacle();
+    }
+
+    void SpawnObstacle() {
+        if (obstaclePrefab && playerController && !playerController.isGameOver)
+        {
+            Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+            Invoke("SpawnObstacle", Random.Range(spawnDelayMin, spawnDelayMax));
+        }
+    }
+}

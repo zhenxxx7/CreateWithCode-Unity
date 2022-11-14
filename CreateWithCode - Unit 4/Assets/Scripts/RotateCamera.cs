ef32ef5576a93d0fbf1f1b536dfb232864c726c1
zhenxxx7 <jordy.zeventh@gmail.com>
2022-11-14 08:02:42 +0700

    add unit 4

---
+++ CreateWithCode - Unit 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] private float axisRotationSpeed = 0;
+    [SerializeField] private float mouseRotationSpeed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal") * axisRotationSpeed;
+        float mInput = Input.GetAxis("Mouse X") * mouseRotationSpeed;
+
+        float input = hInput != 0 ? hInput : mInput;
+
+        transform.Rotate(Vector3.up * input * Time.deltaTime);
+    }
+}

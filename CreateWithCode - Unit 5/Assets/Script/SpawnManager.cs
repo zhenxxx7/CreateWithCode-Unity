1cd8388942878d909a736893b62e866fb67f9387
zhenxxx7 <jordy.zeventh@gmail.com>
2022-11-27 21:04:29 +0700

    add assets

---
+++ CreateWithCode - Unit 5/Assets/Script/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private float baseSpawnInterval = 2;
+    [SerializeField] private GameObject[] TargetPrefabs = null;
+
+    void SpawnTarget()
+    {
+        if (TargetPrefabs != null)
+        {
+            int idx = Random.Range(0, TargetPrefabs.Length);
+
+            if (TargetPrefabs[idx])
+            {
+                Instantiate(TargetPrefabs[idx]);
+            }
+        }
+    }
+
+    public void StartSpawning(int difficulty)
+    {
+        if (difficulty < 1) difficulty = 1;
+        InvokeRepeating("SpawnTarget", 0, baseSpawnInterval / difficulty);
+    }
+
+    public void StopSpawning()
+    {
+        CancelInvoke("SpawnTarget");
+    }
+}
---
+++ CreateWithCode - Unit 5/Assets/Script/Target.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] private float forceMin = 12;
+    [SerializeField] private float forceMax = 16;
+
+    [SerializeField] private float torqueMax = 10;
+
+    [SerializeField] private float initialY = -2;
+    [SerializeField] private float initialPosXRange = 4;
+
+    [SerializeField] private int score = 10;
+
+    [SerializeField] private ParticleSystem explosionParticle;
+
+    private GameManager gameManager = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+
+        transform.position = new Vector3(Random.Range(-initialPosXRange, initialPosXRange), initialY, 0);
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(Vector3.up * Random.Range(forceMin, forceMax), ForceMode.Impulse);
+        rb.AddTorque(new Vector3(Random.Range(-torqueMax, torqueMax), Random.Range(-torqueMax, torqueMax), Random.Range(-torqueMax, torqueMax)), ForceMode.Impulse);
+    }
+
+    void OnMouseDown()
+    {
+        if (gameManager?.IsGameOver() == true) return;
+
+        if (explosionParticle)
+        {
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
+
+        AudioClip clip = GetComponent<AudioSource>()?.clip;
+
+        if (clip)
+        {
+            AudioSource.PlayClipAtPoint(clip, Vector3.zero);
+        }
+
+        Destroy(gameObject);
+
+        gameManager?.AddScore(score);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        if (CompareTag("Good"))
+        {
+            gameManager?.GameOver();
+        }
+    }
+}
